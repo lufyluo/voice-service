45411c7790ff47738c1444984e3fd91e4c2c0416
lufyluo <lufyl@163.com>
2018-06-22 15:42:52 +0800

    1、添加资源路径

--- src/main/java/com/voice/service/domain/entity/VoiceResource.java
+++ src/main/java/com/voice/service/domain/entity/VoiceResource.java
@@ -15,6 +15,15 @@ public class VoiceResource extends RecordModifyEntity {
     private int seriesId;
     private long length;
     private long size;
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 
     public String getTitle() {
         return title;
